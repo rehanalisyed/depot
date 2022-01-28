4b1fcb6688340943b64429b7ba39ab64b4848950
rehan <rehan.syed.kazi@gmail.com>
2022-01-28 11:28:39 +0500

    smart cart

---
+++ app/models/line_item.rb
@@ -0,0 +1,13 @@
+class LineItem < ApplicationRecord
+  
+  belongs_to :order, optional: true
+
+  belongs_to :product
+  belongs_to :cart
+  
+  def total_price
+    product.price * quantity
+  end
+
+  
+end
